fbb625fc43453e5c590e0e1848c38bd2adac598a
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:37:15 +0530

    Add files via upload

---
+++ ap7.c
@@ -0,0 +1,16 @@
+//PROGRAM TO REVERSE AN ARRAY
+#include<stdio.h>
+int main(){
+    int n;         //n is the size of array
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("THE REVERSED ARRAY IS = \n");
+    for(int j=n-1;j>=0;j--){
+        printf("%d ",a[j]);
+    }
+    return 0;
+}
